10b6ea1f03d98c300cfc8b483f71ac818775d460
Taavi Rübenhagen <112179792+taavirubenhagen@users.noreply.github.com>
2022-12-22 21:58:30 +0100

    Untitled

--- .svelte-kit/output/server/index.js
+++ .svelte-kit/output/server/index.js
@@ -2412,7 +2412,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1671742513296"
+      version: "1671742700045"
     };
   }
   async init({ env }) {
