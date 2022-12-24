222bfc79351a89605ad510882079d0b0936c7887
Taavi Rübenhagen <112179792+taavirubenhagen@users.noreply.github.com>
2022-12-24 14:54:22 +0100

    Untitled

--- .svelte-kit/output/server/nodes/2.js
+++ .svelte-kit/output/server/nodes/2.js
@@ -2,7 +2,7 @@
 
 export const index = 2;
 export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/_page.svelte-450b6f77.js';
-export const imports = ["_app/immutable/components/pages/_page.svelte-450b6f77.js","_app/immutable/chunks/index-37bd3a1a.js","_app/immutable/chunks/global-ad420e01.js"];
-export const stylesheets = ["_app/immutable/assets/global-add40282.css"];
+export const file = '_app/immutable/components/pages/_page.svelte-c561d77f.js';
+export const imports = ["_app/immutable/components/pages/_page.svelte-c561d77f.js","_app/immutable/chunks/index-37bd3a1a.js","_app/immutable/chunks/global-ad420e01.js"];
+export const stylesheets = ["_app/immutable/assets/global-52e1ddc9.css"];
 export const fonts = [];
