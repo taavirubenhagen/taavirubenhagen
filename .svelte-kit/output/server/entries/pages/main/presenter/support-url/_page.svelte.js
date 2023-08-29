32b560b6d8df8255f590e910253c6b0c51e0b58a
Taavi Rübenhagen <112179792+taavirubenhagen@users.noreply.github.com>
2023-08-29 21:51:25 +0200

    Untitled

--- .svelte-kit/output/server/entries/pages/main/presenter/support-url/_page.svelte.js
+++ .svelte-kit/output/server/entries/pages/main/presenter/support-url/_page.svelte.js
@@ -1,6 +1,6 @@
 import { c as create_ssr_component } from "../../../../../chunks/index3.js";
 const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<main>Presentation Master 2 lets you remotely control a Powerpoint Presentation playing on your PC while having speaker notes and a presentation timer on your phone.
+  return `<main class="min-h-screen p-8 pt-16">Presentation Master 2 lets you remotely control a Powerpoint Presentation playing on your PC while having speaker notes and a presentation timer on your phone.
     <br><br>
     
     Contact us under tavyai.apps@gmail.com.
--- src/routes/main/presenter/support-url/+page.svelte
+++ src/routes/main/presenter/support-url/+page.svelte
@@ -4,7 +4,7 @@
 
 
 
-<main>
+<main class="min-h-screen p-8 pt-16">
     Presentation Master 2 lets you remotely control a Powerpoint Presentation playing on your PC while having speaker notes and a presentation timer on your phone.
     <br/><br/>
     <!--TODO: 18-->
